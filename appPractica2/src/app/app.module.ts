b2f9a43389377a36765feff384419cf5adde8984
Docente-Lab <davidserrudoclases@gmail.com>
2025-03-11 20:17:44 -0400

    proyecto angular creado

---
+++ appPractica2/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ appPractica2/src/app/pages/acercade/acercade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acercade',
+  standalone: false,
+  templateUrl: './acercade.component.html',  
+})
+export class AcercadeComponent {
+
+}
